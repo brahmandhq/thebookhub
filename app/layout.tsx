cbc654b488a27a77454e70a32eaeda1a8adf4d88
Harsh Agrawal <itsharshag@gmail.com>
2024-05-21 18:10:12 +0530

    Add GoogleAnalytics to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,17 @@ import Sidebar from "../components/Sidebar";
 import Player from "@/components/Player";
 import Providers from "@/components/Provider/Provider";
 import ModalProvider from "@/components/Provider/ModalProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { LayoutGroup } from "framer-motion";
 import Loader from "@/components/Loader";
+
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "TheBookHub",
   description: "Listen To Thousands Of Audio Books For Free",
   icons: {
-    icon: '/icon.ico',
+    icon: "/icon.ico",
   },
 };
 
@@ -26,11 +28,11 @@ export default function RootLayout({
     <html lang="en">
       <link rel="icon" href="/icon.ico" sizes="any" />
       <Providers>
-        <body className={font.className }>
-          <ModalProvider/>
+        <body className={font.className}>
+          <ModalProvider />
           <Sidebar>{children}</Sidebar>
-          
         </body>
+        <GoogleAnalytics gaId="G-X3TYS368W2" />
         <Player />
       </Providers>
     </html>
