a02d0b507c524ccbf15abd2a52f721fcac65c5fa
Ronit Raj <ronitrajofficial7@gmail.com>
2024-05-24 10:38:46 +0530

    fix: will not show hours if its not

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -23,7 +23,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ books }) => {
         className="
         grid 
         grid-cols-1 
-        sm:grid-cols-2 xl:grid-cols-3 gap-4 mt-4 mx-4"
+        sm:grid-cols-2 xl:grid-cols-3 gap-4 mt-4 mx-[1.5rem]"
       >
         {genres.map((genre) => (
           <ListItem
