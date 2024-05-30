43b539f731eb59bb42401df7e1dfaf1406c19485
Ronit Raj <ronitrajofficial7@gmail.com>
2024-05-30 12:22:48 +0530

    fix: search responsiveness in mobile

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -6,12 +6,15 @@ import { useState } from "react";
 import ListItem from "@/components/ListItem";
 import { genres } from "@/data/genre";
 import MoreGenreButton from "@/components/MoreGenreButton";
+import { useMediaQuery } from "react-responsive";
+
 interface SearchContentProps {
   books: any;
 }
 
 const SearchContent: React.FC<SearchContentProps> = ({ books }) => {
   const router = useRouter();
+  const isMobile = useMediaQuery({ maxWidth: 640 });
 
   const handleClick = (id: any) => {
     router.push(`/${id}`);
@@ -51,7 +54,7 @@ const SearchContent: React.FC<SearchContentProps> = ({ books }) => {
   }
 
   return (
-    <div className="flex flex-col gap-y-2 w-full px-6 mb-[7rem]">
+    <div className="flex flex-col gap-y-2 w-full px-0 sm:px-3 md:px-6 mb-[7rem]">
       {books.map((book: any) => (
         <div
           key={book.id}
@@ -67,11 +70,9 @@ const SearchContent: React.FC<SearchContentProps> = ({ books }) => {
           shadow-lg 
           hover:shadow-xl 
           outline-none 
-
         "
           onClick={() => handleClick(book.id)}
         >
-          {" "}
           <div
             className="
           relative 
@@ -79,23 +80,23 @@ const SearchContent: React.FC<SearchContentProps> = ({ books }) => {
           justify-between
           rounded-md 
           h-[52px] 
-          
           w-full
         "
           >
-            <div className="flex flex-col gap-y-1 ">
-              <p className="text-white  text-xl font-bold">{book.name}</p>
-              <p className="text-zinc-400 text-md  font-medium">
+            <div className="flex flex-col gap-y-1">
+              <p className="text-white sm:text-xl text-md font-bold">
+                {isMobile && book.name.length > 37
+                  ? `${book.name.substring(0, 37)}...`
+                  : book.name}
+              </p>
+              <p className="text-zinc-400 sm:text-md text-sm font-medium">
                 By {book.author.firstName + " " + book.author.lastName}
               </p>
             </div>
-            <div className="flex flex-col gap-y-1 items-end ">
-              <p className="text-white  text-sm font-light">
-                {" "}
-                language: {book.language}
-              </p>
-              <p className="text-zinc-400 text-md flex gap-2 items-center  font-medium">
-                <GiDuration /> : {book.duration}
+            <div className="flex flex-col gap-y-1 items-end">
+              <p className="text-white text-sm font-light">{book.language}</p>
+              <p className="text-zinc-400 text-md flex gap-2 items-center font-medium">
+                {book.duration}
               </p>
             </div>
           </div>
